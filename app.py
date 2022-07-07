f1e262cd4060f53c33ae81c00a019014f7e1b9b3
ikathuria <ishani@kathuria.net>
2022-07-07 22:57:22 +0530

    update

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-"""set FLASK_ENV=development"""
-
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request, render_template, Response, send_from_directory, url_for
-import json
 import pandas as pd
-from justdial import scrape_justdial
+from scripts.justdial import scrape_justdial
+from scripts.github_pins import scrape_github
 
 UPLOAD_DIR = 'static/data/'
 
@@ -15,41 +13,50 @@ def home():
     return render_template('index.html', page="home")
 
 
-@app.route("/justdial/")
-def justdial():
+@app.route('/github', methods=["GET", "POST"])
+def get_github_input():
+    if request.method == "POST":
+        username = request.form['git-user']
+
+        # making sure its not empty
+        if username != '':
+            data = scrape_github(username)
+
+            return jsonify(data)
+
+    return render_template('github.html', page="github")
+
+
+@app.route('/justdial', methods=["GET", "POST"])
+def get_justdial_input():
+    if request.method == "POST":
+        topic = request.form['topic']
+        cities = request.form['cities']
+
+        # making sure its not empty
+        if topic != '' and cities != '':
+            topic = topic.lower()
+            cities = cities.split('\n')
+            data = scrape_justdial(topic, cities)
+            df = pd.DataFrame.from_dict(data, orient='index').T
+            df.to_csv(UPLOAD_DIR + 'justdial.csv', index=False)
+
+            return send_from_directory(UPLOAD_DIR, 'justdial.csv')
+
     return render_template('justdial.html', page="justdial")
 
 
-@app.route('/justdial/', methods=["POST"])
-def get_justdial_input():
-    topic = request.form['topic']
-    cities = request.form['cities']
-
-    # making sure its not empty
-    if topic != '' and cities != '':
-        topic = topic.lower()
-        cities = cities.split('\n')
-        data = scrape_justdial(topic, cities)
-        df = pd.DataFrame.from_dict(data, orient='index').T
-        df.to_csv(UPLOAD_DIR + 'justdial.csv', index=False)
-
-        return send_from_directory(UPLOAD_DIR, 'justdial.csv')
-
-    else:
-        return render_template('justdial.html', page="justdial")
-
-
-@app.route("/amazon/")
+@app.route('/amazon')
 def amazon():
     return render_template('amazon.html', page="amazon")
 
 
-@app.route("/myntra/")
+@app.route('/myntra')
 def myntra():
     return render_template('myntra.html', page="myntra")
 
 
-@app.route("/google/")
+@app.route('/google')
 def google():
     return render_template('google.html', page="google")
 
---
+++ scripts/github_pins.py
@@ -0,0 +1,124 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+
+def get_website_image(owner, repo, link):
+    DRIVER.get(link)
+
+    try:
+        about = DRIVER.find_element(By.CLASS_NAME, "BorderGrid-cell")
+        web = about.find_element(By.TAG_NAME, "a").get_attribute("href")
+    except:
+        web = ""
+
+    try:
+        image = DRIVER.find_element(
+            By.NAME, "twitter:image:src"
+        ).get_attribute("content")
+    except:
+        image = f"https://opengraph.githubassets.com/1/{owner}/{repo}"
+
+    return web, image
+
+
+def get_repo(username, pin):
+    try:
+        owner = pin.find_element(By.CLASS_NAME, "owner").text
+    except:
+        owner = username
+
+    try:
+        repo = pin.find_element(By.CLASS_NAME, "repo").text
+    except:
+        return {}
+
+    try:
+        link = f"https://github.com/{owner}/{repo}"
+    except:
+        link = ""
+
+    try:
+        description = pin.find_element(By.CLASS_NAME, "pinned-item-desc").text
+    except:
+        description = ""
+
+    try:
+        language = pin.find_element(By.CLASS_NAME, "pinned-item-desc").text
+    except:
+        language = ""
+
+    try:
+        languageColor = pin.find_element(
+            By.CLASS_NAME, "repo-language-color"
+        ).get_attribute("style")
+        languageColor = languageColor.replace("background-color: ", "")
+        languageColor = languageColor.replace(";", "")
+    except:
+        languageColor = ""
+
+    try:
+        stars = pin.find_element(By.PARTIAL_LINK_TEXT, '/stargazers').text
+    except:
+        stars = "0"
+
+    try:
+        forks = pin.find_element(By.PARTIAL_LINK_TEXT, '/network/members').text
+    except:
+        forks = "0"
+
+    result = {
+        'owner': owner,
+        'repo': repo,
+        'link': link,
+        'description': description,
+        'image': '',
+        'website': '',
+        'language': language,
+        'languageColor': languageColor,
+        'stars': stars,
+        'forks': forks,
+    }
+
+    return result
+
+
+def scrape_github(username):
+    global DRIVER
+
+    data = []
+    DRIVER = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    try:
+        path = f"https://www.github.com/{username}"
+        DRIVER.get(path)
+        DRIVER.set_page_load_timeout(5)
+
+        # getting all details
+        pins = DRIVER.find_elements(
+            By.CLASS_NAME, "pinned-item-list-item-content"
+        )
+
+        for pin in pins:
+            data.append(get_repo(username, pin))
+
+        for i in range(len(data)):
+            data[i]['website'], data[i]['image'] = get_website_image(
+                data[i]['owner'], data[i]['repo'], data[i]['link']
+            )
+
+    except:
+        pass
+
+    DRIVER.quit()
+
+    return data
